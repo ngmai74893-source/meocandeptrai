7288ffc2d2fe2022ee16fe5af311165dac2795b5
ngmai74893-source <ngmai74893@gmail.com>
2025-10-27 13:50:35 +0700

    Update code1.js

--- trangtinh/code1.js
+++ trangtinh/code1.js
@@ -1,1 +1,1 @@
-const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbyw9OFg4wgzcgr5gyesb1rhUL7xGp3pl_ph9AUbjAkumGwBNPOt2Go_b3a92DpqeDMjbg/exec";
+const scriptURL = "https://script.google.com/macros/s/AKfycbyw9OFg4wgzcgr5gyesb1rhUL7xGp3pl_ph9AUbjAkumGwBNPOt2Go_b3a92DpqeDMjbg/exec";
